88820ba4dcff3174322f839d244a54aa0d128f15
NetherForce <nederforce@gmail.com>
2022-09-26 23:17:51 +0300

    register and jwt authorization

--- config/db.js
+++ config/db.js
@@ -5,32 +5,12 @@ const config = require('config');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const fs = require('fs');
 
-const credentials = config.get('mongoPath');
-
-const client = new MongoClient('mongodb+srv://mern-tutorial-cluster.aumcfpy.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
-  sslKey: credentials,
-  sslCert: credentials,
-  serverApi: ServerApiVersion.v1
-});
-
-async function run() {
-  try {
-    await client.connect();
-    // const database = client.db("MERN-Tutorial-Cluster");
-    // const collection = database.collection("testCol");
-    // perform actions using client
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
-// run().catch(console.dir);
+const db = config.get('mongoURI');
 
 
 const connectDB = async () => {
     try {
-        await run().catch(console.dir);
+        await mongoose.connect(db);
         console.log('MongoDB Connected.');
     } catch(err) {
         console.error(err.message);
